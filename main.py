696839d79c5ddc8c3907a26976f3a4b1eab0001f
yonefactory <yonefactory@gmail.com>
2025-02-16 11:29:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import requests
+from datetime import datetime
 
 BOT_TOKEN = "7802394343:AAHqzzij5wD5ms_JkYN3oWz6VvICPMYJNCo"
 CHAT_ID = "7562974684"
 
 def send_message():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    message = f"현재 시간: {current_time}"
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {"chat_id": CHAT_ID, "text": "Hello from GitHub Actions!"}
+    payload = {"chat_id": CHAT_ID, "text": message}
+    
     response = requests.post(url, data=payload)
     if response.status_code == 200:
-        print("Message sent successfully")
+        print("메시지 전송 성공!")
     else:
-        print(f"Failed to send message: {response.text}")
+        print(f"메시지 전송 실패: {response.text}")
 
 if __name__ == "__main__":
     send_message()
