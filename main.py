e2729c59918f5b7a51b568e97b43a866de07e1d8
yonefactory <yonefactory@gmail.com>
2025-02-16 13:22:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 BOT_TOKEN = "7802394343:AAHqzzij5wD5ms_JkYN3oWz6VvICPMYJNCo"
 #CHAT_ID = "7562974684"
-CHAT_ID = -4738445656"
+CHAT_ID = "-4738445656"
 def send_message():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message = f"현재 시간: {current_time}"
